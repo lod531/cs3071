c7031e12406856cdedb9268d2f0e468b24c8676a
Andy <buinovsa@tcd.ie>
2015-10-14 13:24:33 -0700

    Final lexanalyser commit (or is it?)

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,7 +4,10 @@
 
 
 /*
-SEE DOCUMENTATION AT 
+
+
+SEE DOCUMENTATION AT https://github.com/lod531/cs3071/tree/master/lab2
+
 
 */
 const char 	digitChars[24] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
